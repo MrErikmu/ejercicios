7f3542d369a1c90ac08766ed4eea33bdf3de24a5
Erik Moreno Uceda <mrerikmoreno@gmail.com>
2026-02-25 18:37:03 +0100

    Commando ls solo con nombres y fecha de creacion

---
+++ comando ls/comando ls/Command.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+
+namespace comando_ls;
+
+public static class Command
+{
+    public static void ejecutar(string ruta)
+    {
+        if (!Path.Exists(ruta))
+        {
+            Console.WriteLine("La ruta introducida no existe");
+        }
+        else
+        {
+            Console.WriteLine($"{ruta}");
+            Console.Write(ProcesarComando(ruta));
+        }
+    }
+
+    public static string ProcesarComando(string ruta)
+    {
+        StringBuilder sb = new StringBuilder();
+        var carpetacontenido = Directory.GetFiles(ruta);
+        var carpetadirectorios = Directory.GetDirectories(ruta);
+        
+        string fecha;
+        string nombre;
+        string? carpetan;
+        foreach (var contenido in carpetacontenido)
+        {
+            nombre = Path.GetFileName(contenido);
+            fecha = File.GetLastWriteTime(contenido).ToString(CultureInfo.InvariantCulture);
+            sb.AppendLine($"|-> {fecha} {nombre}");
+        }
+        foreach (var contenido in carpetadirectorios)
+        {
+            nombre = Path.GetFileName(contenido);
+            fecha = File.GetLastWriteTime(contenido).ToString(CultureInfo.InvariantCulture);
+            sb.AppendLine($"|-> {fecha} {nombre}");
+        }
+        return sb.ToString();
+    }
+}
---
+++ comando ls/comando ls/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using comando_ls;
+
+Command.ejecutar(@"C:\Users\emu914\Desktop");
